d3903955107c091639813fc2c901ba3761f1eea2
nmaynard <nmaynard@mitre.org>
2022-10-03 17:55:43 -0400

    update the git

---
+++ src/components/AuthenticationLayout.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+<!-- <h1>Test</h1> -->
+<q-img height="100vh" src="../assets/login-background.jpeg">
+</q-img>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AuthenticationLayout'
+});
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,61 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <span class="text-secondary">Salary</span> Stone
+          <q-avatar>
+            <img src="../assets/rosetta-stone2.svg">
+          </q-avatar>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer bordered class="bg-primary text-white">
+      <div class="row no-wrap shadow-1">
+      <q-toolbar class="col-11">
+        <q-btn flat round dense icon="copyright" />
+          <div>2022 George Mason University. All Rights Reserved.</div>
+      </q-toolbar>
+      <q-toolbar class="col-1">
+      <router-view>test</router-view>
+        <q-btn flat round dense icon="menu" />
+        <q-btn flat round dense icon="menu" />
+        <q-btn flat round dense icon="menu" />
+      </q-toolbar>
+      </div>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
